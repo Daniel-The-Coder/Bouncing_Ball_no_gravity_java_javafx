f8d28cb45a1c6d3de212254be420e1978d1e3b45
dxr3977 <dxr3977@g.rit.edu>
2016-07-10 10:33:43 -0400

    Created a module for a similar program, but with 2 balls instead of one.

---
+++ two_balls/src/BallsGUI.java
@@ -0,0 +1,98 @@
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.*;
+
+/**
+ * Created by Lord Daniel on 7/10/2016.
+ */
+public class BallsGUI extends Application implements Observer{
+    private BallsModel model;
+    private Pane canvas;
+    private Circle circle;
+    private Scene scene;
+    private  Text txt = new Text("0");
+
+    @Override
+    public void init() throws Exception{
+        this.model = new BallsModel();
+        model.addObserver( this );
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        scene = new Scene(root, model.getMaxX(), model.getMaxY());
+        stage.setScene(scene);
+        stage.setTitle("Bouncing Ball");
+
+        VBox vb = new VBox();
+
+        HBox hb = new HBox();
+        Button start = new Button("Start");
+        start.setOnAction(event -> model.simulateTime());
+        hb.getChildren().add(start);
+
+        Button decrVel = new Button("- velocity");
+        decrVel.setOnAction(event -> model.decrVel());
+        hb.getChildren().add(decrVel);
+
+        Button incrVel = new Button("+ velocity");
+        incrVel.setOnAction(event -> model.incrVel());
+        hb.getChildren().add(incrVel);
+
+        vb.getChildren().add(hb);
+        vb.getChildren().add(txt);
+
+        canvas = new Pane();
+        circle = new Circle(model.radius);
+        circle.setCenterX(model.getX());
+        circle.setCenterY(model.getY());
+        circle.setFill(Paint.valueOf("#ff0000"));
+        canvas.getChildren().add(circle);
+
+        vb.getChildren().add(canvas);
+
+        scene.setRoot(vb);
+        stage.setResizable(false);
+        stage.show();
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                txt.setText("X: "+model.getX()+"; Y: "+model.getY()+";   speed: "+model.speed+";   X velocity: "+model.Xdirection*model.speed+"; Y velocity: "+model.Ydirection*model.speed);
+                circle.setCenterX(model.getX());
+                circle.setCenterY(model.getY());
+                circle.setFill(model.getColor());
+                System.out.println(model.getX()+" "+model.getY());
+                model.simulateTime();
+            }
+        }, 0, 20);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ two_balls/src/BallsModel.java
@@ -0,0 +1,108 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Observable;
+import javafx.scene.paint.Paint;
+
+/**
+ * Created by Lord Daniel on 7/10/2016.
+ */
+public class BallsModel extends Observable {
+    public int Xdirection = 1;
+    public int Ydirection = 1;
+    public int speed = 8;
+    private int x;
+    private int y;
+    private int maxX = 700;
+    private int maxY = 500;
+    private boolean flag = true;
+    public int radius = 30;
+    private int colorIndex;
+
+    private ArrayList<Paint> colors;
+
+    public BallsModel(){
+        this.x = 100;
+        this.y = 100;
+        this.colorIndex = 0;
+
+        this.colors = new ArrayList<>();
+        colors.add(Paint.valueOf("#ff0000"));
+        colors.add(Paint.valueOf("#0000ff"));
+        colors.add(Paint.valueOf("#912cee"));
+        colors.add(Paint.valueOf("#00ff00"));
+        colors.add(Paint.valueOf("#ff4500"));
+        colors.add(Paint.valueOf("#7171c6"));
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public  int getMaxX(){
+        return this.maxX;
+    }
+
+    public int getMaxY(){
+        return this.maxY;
+    }
+
+    public Paint getColor(){
+        return this.colors.get(this.colorIndex);
+    }
+
+    private void nextColor(){
+        if(this.colorIndex == this.colors.size()-1){
+            this.colorIndex = 0;
+        }
+        else{
+            this.colorIndex++;
+        }
+    }
+
+    public void decrVel(){
+        if(this.speed > 0){
+            speed--;
+        }
+    }
+
+    public void incrVel(){
+        speed++;
+    }
+
+    public void simulateTime(){
+        //update velocity based on position
+        //X velocity
+        if(this.x + this.radius >= this.getMaxX()){
+            this.Xdirection *= -1;
+            nextColor();
+        }
+        else if(this.x - this.radius <= 0){
+            this.Xdirection *= -1;
+            nextColor();
+        }
+        //Y velocity
+        if(this.y + 2*this.radius >= this.getMaxY()){
+            this.Ydirection *= -1;
+            nextColor();
+        }
+        else if(this.y - this.radius <= 0){
+            this.Ydirection *= -1;
+            nextColor();
+        }
+
+        //update position based on velocity
+        this.x += this.Xdirection*speed;
+        this.y += this.Ydirection*speed;
+        if(flag == true) {
+            setChanged();
+            notifyObservers();
+            flag=false;
+        }
+
+    }
+
+}
